d16539bbf9dd33a84f21fdfd449b95968d625864
praxx <rjyg7qrs7d@privaterelay.appleid.com>
2026-05-06 19:46:36 +0300

    Update test_gamykla.py

--- tests/test_gamykla.py
+++ tests/test_gamykla.py
@@ -8,25 +8,27 @@ from src.gamykla import RusiuokliuGamykla
 
 class TestRusiuokliuGamykla(unittest.TestCase):
 
+    def setUp(self):
+        self.gamykla = RusiuokliuGamykla()
+
     def test_sukurti_burbulu(self):
-        rusiuoklis = RusiuokliuGamykla.sukurti("burbulu")
+        rusiuoklis = self.gamykla.sukurti("burbulu")
         self.assertIsInstance(rusiuoklis, BurbuluRusiavimas)
 
     def test_sukurti_greitaji(self):
-        rusiuoklis = RusiuokliuGamykla.sukurti("greitasis")
+        rusiuoklis = self.gamykla.sukurti("greitasis")
         self.assertIsInstance(rusiuoklis, GreitasisRusiavimas)
 
     def test_didziosios_raides(self):
-        # Didzioji ar mazoji raide neturi reiksmes.
-        rusiuoklis = RusiuokliuGamykla.sukurti("BURBULU")
+        rusiuoklis = self.gamykla.sukurti("BURBULU")
         self.assertIsInstance(rusiuoklis, BurbuluRusiavimas)
 
     def test_nezinomas_algoritmas(self):
         with self.assertRaises(ValueError):
-            RusiuokliuGamykla.sukurti("nezinomas")
+            self.gamykla.sukurti("nezinomas")
 
     def test_galimu_algoritmu_sarasaas(self):
-        galimi = RusiuokliuGamykla.galimi_algoritmai()
+        galimi = self.gamykla.galimi_algoritmai()
         self.assertIn("burbulu", galimi)
         self.assertIn("greitasis", galimi)
         self.assertIn("iterpimo", galimi)
